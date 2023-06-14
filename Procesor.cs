80f683d5873118e4936bb074f85dd70a9ebb1ff7
vababi <vlad.2002.ababi@gmail.com>
2023-06-14 19:06:50 +0300

    Add project files.

---
+++ Procesor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Componente_PC
+{
+    internal class Procesor
+    {
+        public string CodProcesor { get;set; }
+        public string NumeProcesor { get;set;}
+        public int NrNucleuProcesor { get;set; } 
+        public double PretProcesor { get;set; }  
+        public string ProducatorProcesor { get;set; } 
+    
+     public Procesor()
+        {
+
+        }
+     public Procesor(string codProcesor, string numeProcesor, int nrNucleuProcesor, double pretProcesor, string producatorProcesor)
+        {
+            CodProcesor = codProcesor;
+            NumeProcesor = numeProcesor;
+            NrNucleuProcesor = nrNucleuProcesor;
+            PretProcesor = pretProcesor;
+            ProducatorProcesor = producatorProcesor;
+        }
+    }
+}
